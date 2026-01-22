d2b1a2ba0c1a22ea984da43854e872212b4fcaf1
beepek <ben.schylling@gmail.com>
2026-01-22 14:38:11 -0800

    animation for welcome page elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'slide-in-left': {
+          '0%': {
+            transform: 'translateX(-100%)',
+            opacity: '0',
+          },
+          '100%': {
+            transform: 'translateX(0)',
+            opacity: '1',
+          },
+        },
+      },
+      animation: {
+        'slide-in-left': 'slide-in-left 0.8s ease-out',
+      },
+    },
   },
   plugins: [require('@tailwindcss/container-queries')],
 }
